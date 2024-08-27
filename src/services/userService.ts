4d595d1ac6ea9faf86b399b37a89f857a548487a
Bojan Misic <bojan.misic@awesome7.com>
2024-08-27 12:04:05 +0200

    Add user vertical and setup server and routes

---
+++ src/services/userService.ts
@@ -0,0 +1,25 @@
+// src/services/userService.ts
+import { User } from "../model/users/user";
+
+// A post request should not contain an id.
+export type UserCreationParams = Pick<User, "email" | "name" | "phoneNumbers">;
+
+export class UserService {
+  public get(id: number, name?: string): User {
+    return {
+      id,
+      email: "jane@doe.com",
+      name: name ?? "Jane Doe",
+      status: "Happy",
+      phoneNumbers: [],
+    };
+  }
+
+  public create(userCreationParams: UserCreationParams): User {
+    return {
+      id: Math.floor(Math.random() * 10000), // Random
+      status: "Happy",
+      ...userCreationParams,
+    };
+  }
+}
