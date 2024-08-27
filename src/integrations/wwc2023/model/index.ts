11a9595047dc25b6a036ab5ff27018f52115ae11
Bojan Misic <bojan.misic@awesome7.com>
2024-08-27 18:13:43 +0200

    Integrate FIFA WWC API

---
+++ src/integrations/wwc2023/model/index.ts
@@ -0,0 +1,8 @@
+export { BlTableTeam } from "./bl-table-team";
+export { Match } from "./match";
+export { ResultInfo } from "./result-info";
+export { Sport } from "./sport";
+export { Team } from "./team";
+export { Group } from "./group";
+export { League } from "./league";
+export { GoalGetter } from "./goal-getter";
